269e7cda5c6dfe0ca1e81a06f51f7f6b508cfc3d
Douglas Fugita <douglas.cezar@gmail.com>
2023-01-12 03:37:05 -0300

    Common.Logging - SeriLogger unit test

---
+++ src/BuildingBlocks/BuildingBlocks.Tests/Logging/SeriLoggerTest.cs
@@ -0,0 +1,32 @@
+﻿using Common.Logging;
+using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Tests.Logging;
+public class SeriLoggerTest
+{
+    [Fact]
+    public void Create_OnSuccess_ShouldGenerateElasticConfiguration()
+    {
+        //Arrange
+        var builder = WebApplication.CreateBuilder();
+        builder.Configuration.AddJsonFile("./Logging/appsettingsfake.json");
+
+        //Act
+        builder.Host.UseSerilog(SeriLogger.Configure);
+
+        // Assert
+        var app = builder.Build();
+        app.Logger.GetType().FullName.Should().Be("Serilog.Extensions.Logging.SerilogLogger");
+    }
+}
