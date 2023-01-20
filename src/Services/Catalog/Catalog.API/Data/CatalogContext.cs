b324dff50d39b574a3d25e4085b119210d5ef424
Douglas Fugita <douglas.cezar@gmail.com>
2023-01-20 19:54:50 -0300

    Catalog.API - Setup for Telemetry - Metrics and Tracing

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -2,6 +2,7 @@
 using Catalog.API.Entities;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Extensions.DiagnosticSources;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Catalog.API.Data;
@@ -12,7 +13,10 @@ public class CatalogContext : ICatalogContext
 
     public CatalogContext(IOptions<DatabaseSettingsModel> settings)
     {
-        var mongoClient = new MongoClient(settings.Value.ConnectionString);
+        var clientSettings = MongoClientSettings.FromConnectionString(settings.Value.ConnectionString);
+        clientSettings.ClusterConfigurator = cb => cb.Subscribe(new DiagnosticsActivityEventSubscriber());
+        var mongoClient = new MongoClient(clientSettings);
+
         var mongoDatabase = mongoClient.GetDatabase(settings.Value.DatabaseName);
 
         Products = mongoDatabase.GetCollection<Product>(settings.Value.CollectionName);
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -3,20 +3,34 @@ using Catalog.API.Data;
 using Catalog.API.Data.Models;
 using Catalog.API.Repositories;
 using Common.Logging;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog;
 using System.Runtime.CompilerServices;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenTelemetry()
-    .WithMetrics(builder => builder
+    .WithMetrics(metricBuilder => metricBuilder
         .AddAspNetCoreInstrumentation()
-        .AddPrometheusExporter()
-        ).StartWithHost();
-
+        .AddPrometheusExporter())
+    .WithTracing(tracingBuilder => tracingBuilder
+        .AddSource("Catalog")
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName: "Catalog", serviceVersion: "1.0")
+                .AddTelemetrySdk())
+        .AddAspNetCoreInstrumentation()
+        .AddMongoDBInstrumentation()
+        .AddOtlpExporter(options =>
+            options.Endpoint = new Uri(builder.Configuration.GetValue<string>("JaegerConfiguration:Uri")))
+        )
+    .StartWithHost();
 
 builder.Services.AddControllers();
 builder.Services.RegisterServices(builder.Configuration);
@@ -27,6 +41,7 @@ builder.Services.AddSwaggerGen();
 builder.Host.UseSerilog(SeriLogger.Configure);
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
