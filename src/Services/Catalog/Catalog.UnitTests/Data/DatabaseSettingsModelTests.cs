d5b9c616125ca1b1e064ebf501b83a917417bc3c
Douglas Fugita <douglas.cezar@gmail.com>
2023-01-27 17:10:19 -0300

    Catalog.Core - Unit test for DatabaseSettingsModel

---
+++ src/Services/Catalog/Catalog.UnitTests/Data/DatabaseSettingsModelTests.cs
@@ -0,0 +1,39 @@
+﻿using Bogus;
+using Catalog.Core.Data.Models;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.UnitTests.Data;
+public class DatabaseSettingsModelTests
+{
+    [Fact]
+    public void DatabaseSettingsModel_OnCreation_ObjectCreated()
+    {
+
+        //Arrange
+        var settingsFaker = new Faker<DatabaseSettingsModel>()
+            .RuleFor(s => s.ConnectionString, f => f.Random.Uuid().ToString())
+            .RuleFor(s => s.DatabaseName, f => f.Random.Uuid().ToString())
+            .RuleFor(s => s.CollectionName, f => f.Random.Uuid().ToString());
+
+        var settings = settingsFaker.Generate();
+
+        //Act
+        var sut = new DatabaseSettingsModel
+        {
+            ConnectionString = settings.ConnectionString,
+            DatabaseName = settings.DatabaseName,
+            CollectionName = settings.CollectionName,
+        };
+
+        //Assert
+        sut.ConnectionString.Should().Be(settings.ConnectionString);
+        sut.DatabaseName.Should().Be(settings.DatabaseName);
+        sut.CollectionName.Should().Be(settings.CollectionName);
+    }
+
+}
