6c7028cd21f1dc432aa28288fd9adac74747c370
Douglas Fugita <douglas.cezar@gmail.com>
2023-02-08 18:47:55 -0300

    BuildingBlocks - Redis Caching

---
+++ src/BuildingBlocks/Common.Caching/IRedisCacheProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Caching;
+public interface IRedisCacheProvider
+{
+    T? Get<T>(string key);
+    void Set<T>(string key, T value);
+    void Remove<T>(string key);
+}
---
+++ src/BuildingBlocks/Common.Caching/RedisCacheProvider.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Common.Caching;
+public class RedisCacheProvider : IRedisCacheProvider
+{
+    private readonly IDistributedCache _cache;
+
+    public RedisCacheProvider(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public T? Get<T>(string key)
+    {
+        var value = _cache.GetString(key);
+        if (value != null)
+        {
+            return JsonSerializer.Deserialize<T>(value);
+        }
+        return default;
+    }
+
+    public void Set<T>(string key, T value)
+    {
+        var timeout = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(3),
+            SlidingExpiration = TimeSpan.FromMinutes(1),
+        };
+
+        _cache.SetString(key, JsonSerializer.Serialize(value), timeout);
+    }
+
+    public void Remove<T>(string key)
+    {
+        _cache.Remove(key);
+    }
+}
