5a22be0dae447f881b3511df707ce26b1a512911
Douglas Fugita <douglas.cezar@gmail.com>
2023-02-08 22:00:14 -0300

    Catalog Minimal API - Initial RabbitMQ Publisher code

--- src/Services/Catalog/Catalog.Minimal.API/Products/ProductService.cs
+++ src/Services/Catalog/Catalog.Minimal.API/Products/ProductService.cs
@@ -1,6 +1,8 @@
 ﻿using Catalog.Core.Entities;
 using Catalog.Core.Repositories;
 using Common.Caching;
+using RabbitMQ.Client;
+using System.Text.Json;
 
 namespace Catalog.Minimal.API.Products;
 
@@ -9,18 +11,38 @@ public class ProductService : IProductService
     private readonly ICatalogRepository _catalogRepository;
     private readonly Serilog.ILogger _logger;
     private readonly IRedisCacheProvider _cache;
-    public ProductService(ICatalogRepository catalogRepository, IRedisCacheProvider cache, Serilog.ILogger logger)
+
+    private readonly IConnectionFactory _connectionFactory;
+    public ProductService(ICatalogRepository catalogRepository, IRedisCacheProvider cache, Serilog.ILogger logger, IConnectionFactory connectionFactory)
     {
         _catalogRepository = catalogRepository;
         _cache = cache;
         _logger = logger;
+        _connectionFactory = connectionFactory;
     }
 
     public void CreateProduct(Product? product)
     {
         if (product is null) { throw new ArgumentNullException(nameof(product)); }
+
         _catalogRepository.CreateProduct(product);
         _cache.Set<Product>(product.Id, product);
+
+
+        using var connection = _connectionFactory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        var exchange = "catalog_exchange";
+        var queue = "catalog_queue";
+        var routingKey = "catalog_routingkey";
+
+        channel.ExchangeDeclare(exchange, ExchangeType.Direct);
+        channel.QueueDeclare(queue, true, false, false, null);
+        channel.QueueBind(queue, exchange, routingKey);
+
+        var body = System.Text.Encoding.UTF8.GetBytes(JsonSerializer.Serialize(product));
+
+        channel.BasicPublish(exchange, routingKey, null, body);
     }
 
     public void DeleteProduct(string id)
--- src/Services/Catalog/Catalog.Minimal.API/Program.cs
+++ src/Services/Catalog/Catalog.Minimal.API/Program.cs
@@ -1,12 +1,21 @@
 using Catalog.Minimal.API.Configuration;
 using Catalog.Minimal.API.Products;
 using Common.Logging;
+using Polly;
+using RabbitMQ.Client;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.RegisterServices(builder.Configuration);
 
+var connectionFactory = new ConnectionFactory {
+    HostName= "localhost",
+    UserName = "guest",
+    Password= "guest"
+};
+
+builder.Services.AddSingleton<IConnectionFactory>(connectionFactory);
 
 
 builder.Services.AddEndpointsApiExplorer();
