a9a8791e4284d6058d71bdb9ec4ef9f13d50ece2
Douglas Fugita <douglas.cezar@gmail.com>
2023-02-11 13:15:19 -0300

    Catalog Minimal API - Refactored to use CloudAMQP RabbitMQ via secrets.json

--- src/Services/Catalog/Catalog.Minimal.API/Program.cs
+++ src/Services/Catalog/Catalog.Minimal.API/Program.cs
@@ -1,6 +1,7 @@
 using Catalog.Minimal.API.Configuration;
 using Catalog.Minimal.API.Products;
 using Common.Logging;
+using Microsoft.Extensions.Configuration;
 using Polly;
 using RabbitMQ.Client;
 using Serilog;
@@ -10,9 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.RegisterServices(builder.Configuration);
 
 var connectionFactory = new ConnectionFactory {
-    HostName= "localhost",
-    UserName = "guest",
-    Password= "guest"
+    Uri = new Uri(builder.Configuration.GetValue<string>("QueueSettings:ConnectionString"))
 };
 
 builder.Services.AddSingleton<IConnectionFactory>(connectionFactory);
