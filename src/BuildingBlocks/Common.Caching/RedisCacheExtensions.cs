f45483fce586e66883573686c149bb4e35bccf09
Douglas Fugita <douglas.cezar@gmail.com>
2023-02-13 16:14:38 -0300

    Common Caching - Created Redis Extensions to access connection object

---
+++ src/BuildingBlocks/Common.Caching/RedisCacheExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Caching.StackExchangeRedis;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Caching;
+public static class RedisCacheExtensions
+{
+    public static ConnectionMultiplexer GetConnection(this RedisCache cache)
+    {
+        //ensure connection is established
+        typeof(RedisCache).InvokeMember("Connect", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.InvokeMethod, null, cache, new object[] { });
+
+        //get connection multiplexer
+        var fi = typeof(RedisCache).GetField("_connection", BindingFlags.Instance | BindingFlags.NonPublic);
+        var connection = (ConnectionMultiplexer)fi.GetValue(cache);
+        return connection;
+    }
+}
